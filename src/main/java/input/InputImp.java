cc22e0097504d79660fb432f14c3e08d62ea3435
victorlian <vlia679@aucklanduni.ac.nz>
2017-08-01 19:39:01 +1200

    Finish implementing the InputImp
    InputImp now reads input file / command line argument and pre-process as
    required.
    The class has passed the basic tests.
    -- Victor

--- src/main/java/input/InputImp.java
+++ src/main/java/input/InputImp.java
@@ -1,6 +1,8 @@
 package input;
 
-import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,8 +18,8 @@ import interfaces.Input;
  *
  */
 public class InputImp implements Input{
-	private int _nOOfProcessors;
-	private List<String[]> _graphData;
+	private int _noOfProcessors;
+	private List<String[]> _graphData = new ArrayList<String[]>();
 	
 	/**
 	 * The constructor will take arguments directly from the command line.
@@ -25,23 +27,71 @@ public class InputImp implements Input{
 	 * @param noOfProcessors
 	 */
 	public InputImp (String filePath, String noOfProcessors){
-			
+		_noOfProcessors = Integer.parseInt(noOfProcessors);
+		processFile(filePath);
 	}
 
+	/**
+	 * See {@link Input#getProcessorCount()};
+	 */
 	@Override
 	public int getProcessorCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public List<String[]> getGraphData() {
-		// TODO Auto-generated method stub
-		return null;
+		return _noOfProcessors;
 	}
 	
-	private void inputProcess(){
-		Scanner inputScanner = new Scanner (new BufferedInputStream(System.in));
+	/**
+	 * See {@link Input#getGraphData()}
+	 */
+	@Override
+	public List<String[]> getGraphData() {
+		return _graphData;
+	}
+	
+	/**
+	 * This helper method will read from the input file, and create the List<String> needed
+	 * for graph data.
+	 */
+	private void processFile(String filePath){
+		File file = new File (filePath);
+		try {
+			Scanner inputScanner = new Scanner (file);
+			
+			inputScanner.nextLine(); //ignore line1.
+			
+			//Loop to read all lines. Only exit if reaches a single line with closing bracket }.
+			while (true){
+				String line = inputScanner.nextLine();
+				
+				//Final line escape.
+				if (line.trim().equals("}")){
+					break;
+				}
+				
+				String[] lineArray = line.split("\\[");
+				
+				//Processing the name (index 1)
+				lineArray[0] = lineArray[0].trim();				
+				if (lineArray[0].length() == 1){
+					//Do nothing (single node already)
+				}
+				else{ //For an arc
+					String[] arcSplitted = lineArray[0].split("->");
+					lineArray[0] = arcSplitted[0].trim() + " " + arcSplitted[1].trim();
+				}
+				
+				//Processing the weight (index 2)
+				lineArray[1] = lineArray[1].replaceAll("\\D", "");
+				
+				_graphData.add(lineArray);				
+			}
+			
+			inputScanner.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
