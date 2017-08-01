fe86bef5f92d75b99594aff837b9c4f55cc5dd78
dan-wong <dan_wong@gmail.com>
2017-08-01 14:21:45 +1200

    Moving interfaces into the correct package - Daniel

--- src/main/java/interfaces/Node.java
+++ src/main/java/interfaces/Node.java
@@ -1,4 +1,44 @@
-package interfaces;
 
-public interface Node {
+import java.util.List;
+
+/**
+ * Created by st970 (Mike Lee) on 31/07/2017.
+ */
+interface Node {
+    /**
+     * getName() returns the name of the current node.
+     * @return String
+     */
+    String getName();
+
+    /**
+     * getWeight() returns the weight of the current node.
+     * The weight is represented in integers.
+     * @return int
+     */
+    int getWeight();
+
+    /**
+     * addOutArc(Arc arc) adds an outward arc to the current node.
+     * @param arc
+     */
+    void addOutArc(Arc arc);
+
+    /**
+     * addInArc(Arc arc) adds an inward arc to the current node.
+     * @param arc
+     */
+    void addInArc(Arc arc);
+
+    /**
+     * getPredecessors( ) returns the list of predecessor nodes.
+     * @return List<Node>
+     */
+    List<Node> getPredecessors( );
+
+    /**
+     * getSuccessors( ) returns the list of successor nodes.
+     * @return List<Node>
+     */
+    List<Node> getSuccessors( );
 }
