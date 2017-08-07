91fa211f8f5add920c60b30313700c9d8acd5630
Daniel Wong <dan-wong@users.noreply.github.com>
2017-08-08 10:29:47 +1200

    Implemented Algorithim Class  - Recursive schedule generation
    AlgorithmNode class
     - An object to represent a node and core

--- src/main/java/implementations/algorithm/Algorithm.java
+++ src/main/java/implementations/algorithm/Algorithm.java
@@ -3,16 +3,51 @@ package implementations.algorithm;
 import interfaces.DAG;
 import interfaces.Node;
 
-public class Algorithm {
-	public DAG _dag;
+import java.util.ArrayList;
+import java.util.List;
 
-	public Algorithm(DAG dag) {
+/**
+ * This class implements the algorithim to solve the scheduling problem
+ */
+public class Algorithm {
+	private DAG _dag;
+	private int _numberOfCores;
+
+	public Algorithm(DAG dag, int numberOfCores) {
 		_dag = dag;
-		generateSchedule();
+		_numberOfCores = numberOfCores;
+
+		List<Node> allNodes = _dag.getAllNodes();
+		recursiveScheduleGeneration(new ArrayList<>(), AlgorithmNode.convertNodetoAlgorithimNode(allNodes));
 	}
 
-	private void generateSchedule() {
-		Node node = _dag.getAllNodes().get(0);
-		System.out.println(node.getName());
+	/**
+	 * This method recursively generates all possible schedules given a list of nodes.
+	 *
+	 * @param processed      - A list of processed nodes
+	 * @param remainingNodes - A list of nodes remaining to be processed
+	 */
+	private void recursiveScheduleGeneration(List<AlgorithmNode> processed, List<AlgorithmNode> remainingNodes) {
+		//Base Case
+		if (remainingNodes.size() == 0) {
+			for (AlgorithmNode node : processed) {
+				System.out.printf(node.getNodeName() + node.getCore() + " ");
+			}
+			System.out.println();
+		} else {
+			for (int i = 0; i < remainingNodes.size(); i++) {
+				for (int j = 1; j <= _numberOfCores; j++) {
+					List<AlgorithmNode> newProcessed = new ArrayList<>(processed);
+					AlgorithmNode node = remainingNodes.get(i);
+					node.setCore(j);
+					newProcessed.add(node);
+
+					List<AlgorithmNode> newRemaining = new ArrayList<>(remainingNodes);
+					newRemaining.remove(i);
+
+					recursiveScheduleGeneration(newProcessed, newRemaining);
+				}
+			}
+		}
 	}
 }
