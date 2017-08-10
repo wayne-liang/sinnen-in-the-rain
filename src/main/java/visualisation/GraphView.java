f24df0dabd9e61274812d54003fd87d666db97b6
ripperonipepperoni <darius.au96@gmail.com>
2017-08-10 12:28:07 +1200

    Darius - Initial commit GraphView

--- src/main/java/visualisation/GraphView.java
+++ src/main/java/visualisation/GraphView.java
@@ -1,5 +1,21 @@
 package visualisation;
 
+import java.awt.event.ActionListener;
+
+import javax.swing.JTable;
+
+/**
+ * This interface allows different for views using different libraries to be plugged into the 
+ * MVC architecture.
+ * 
+ * @author dariusau
+ *
+ */
 public interface GraphView {
-	public void init();
+	
+	//Adds an ActionListener - the controller of the MVC structure
+	public void addButtonListener(ActionListener listener);
+	
+	//Gets the table - the model of the MVC structure
+	public JTable getTable();
 }
