7dc7333e40227163ba0553616803295a6575b232
TacticalSandwich <wlia631@aucklanduni.ac.nz>
2017-08-10 15:06:39 +1200

    Wayne - Optimised calculateTotalTime and changed getStartTimeFromSchedulerTime method to getIndexOfList

--- src/main/java/implementations/algorithm/Algorithm.java
+++ src/main/java/implementations/algorithm/Algorithm.java
@@ -60,91 +60,60 @@ public class Algorithm {
 	 * @return - SchedulerTime object with cost and execution time information
 	 */
 	public SchedulerTime calculateTotalTime(List<AlgorithmNode> algNodes) {
-		//making a copy that I can manipulate
-		//this copy is going work as a list of nodes that still need their start time calculated
-		List<AlgorithmNode> remainingAlgNodes = new ArrayList<>(algNodes);
 		//creating a corresponding array of Nodes
-		//make sure to remove the corresponding Node object when you remove a AlgNode object
-		List<Node> remainingNodes = new ArrayList<>();
+		List<Node> nodes = new ArrayList<>();
 
 		//populating new nodes array with corresponding Node objects
-		for (AlgorithmNode algNode : remainingAlgNodes) {
-			remainingNodes.add(_dag.getNodeByName(algNode.getNodeName()));
+		for (AlgorithmNode algNode : algNodes) {
+			nodes.add(_dag.getNodeByName(algNode.getNodeName()));
 		}
 
 		//creating ArrayLists to represent the schedule for each core
 		//NOTE: could change the coreSchedule to just an ArrayList that holds the most recently scheduled node for each core
-		ArrayList<ArrayList<AlgorithmNode>> coreSchedules = new ArrayList<>();
-		for (int i = 0; i < _numberOfCores; i++) {
-			coreSchedules.add(new ArrayList<>());
-		}
+		ArrayList<AlgorithmNode> latestAlgNodeInSchedules = new ArrayList<>();
 
 		//creating a SchedulerTime object for holding the schedule start times for each node
 		SchedulerTime st = new SchedulerTime(algNodes);
 
-		while (!remainingAlgNodes.isEmpty()) {
-			//maintained list of AlgNodes and Nodes which need to be removed after each finished for loop
-			List<AlgorithmNode> algNodesToRemove = new ArrayList<>();
-			List<Node> nodesToRemove = new ArrayList<>();
+		//looping through all the AlgorithmNodes to set startTimes
+		for (AlgorithmNode currentAlgNode : algNodes) {
+			Node currentNode = nodes.get(algNodes.indexOf(currentAlgNode));
+			int highestCost = 0;
 
-			for (AlgorithmNode currentAlgNode : remainingAlgNodes) {
-				Node currentNode = remainingNodes.get(remainingAlgNodes.indexOf(currentAlgNode));
-				int highestCost = 0;
-
-				//calculate the highest time delay caused by dependencies
-				for (Node node : currentNode.getPredecessors()) {
-					//get index position of corresponding AlgNode object, to check the start time in SchedulerTime
-					int nodeIndex = 0;
-					for (AlgorithmNode anotherAlgNode : algNodes) {
-						if (anotherAlgNode.getNodeName().equals(node.getName())) {
-							nodeIndex = algNodes.indexOf(anotherAlgNode);
-							break;
-						}
-					}
-
-					//calculating the maximum delay caused by this particular dependent node
-					int cost = st.getNodeStartTime(nodeIndex) + node.getWeight();
-					if (!(algNodes.get(nodeIndex).getCore() == currentAlgNode.getCore())) {
-						//add on arc weight, since they're on different cores
-						cost += currentNode.getInArc(node).getWeight();
-					}
-
-					if (cost > highestCost) {
-						highestCost = cost;
-					}
+			//calculate the highest time delay caused by dependencies
+			for (Node node : currentNode.getPredecessors()) {
+				//calculating the maximum delay caused by this particular dependent node
+				int cost = st.getNodeStartTime(getIndexOfList(node, algNodes)) + node.getWeight();
+				if (!(algNodes.get(getIndexOfList(node, algNodes)).getCore() == currentAlgNode.getCore())) {
+					//add on arc weight, since they're on different cores
+					cost += currentNode.getInArc(node).getWeight();
 				}
 
-				//calculate the highest time delay caused by previous processes on the same core
-				int coreNum = currentAlgNode.getCore();
-				ArrayList<AlgorithmNode> currentCore = coreSchedules.get(coreNum);
-				currentCore.add(currentAlgNode);
-
-				//check when previous process on given core is finished, if there is one
-				if (currentCore.size() > 1) {
-					Node previousNode = _dag.getNodeByName(currentCore.get(currentCore.size() - 2).getNodeName());
-					int cost = previousNode.getWeight() + getStartTimeFromSchedulerTime(previousNode.getName(), algNodes, st);
-					if (cost > highestCost) {
-						highestCost = cost;
-					}
+				if (cost > highestCost) {
+					highestCost = cost;
 				}
-
-				//set SchedulerTime startTime for this node
-				st.setStartTimeForNode(highestCost, algNodes.indexOf(currentAlgNode));
-				algNodesToRemove.add(currentAlgNode);
-				nodesToRemove.add(currentNode);
 			}
 
-			//have to remove the nodes which have successfully had their start times calculated
-			//make sure to remove from both lists
-			remainingAlgNodes.removeAll(algNodesToRemove);
-			remainingNodes.removeAll(nodesToRemove);
+			AlgorithmNode latestNode = latestAlgNodeInSchedules.get(currentAlgNode.getCore());
+			if (latestNode != null) {
+				Node previousNode = _dag.getNodeByName(latestNode.getNodeName());
+				int cost = previousNode.getWeight() + st.getNodeStartTime(algNodes.indexOf(latestNode));
+				if (cost > highestCost) {
+					highestCost = cost;
+				}
+			}
+
+			//set currentAlgNode as the newest node to be scheduled on it's core
+			latestAlgNodeInSchedules.add(currentAlgNode.getCore(), currentAlgNode);
+
+			//set SchedulerTime startTime for this node
+			st.setStartTimeForNode(highestCost, algNodes.indexOf(currentAlgNode));
 		}
 
 		int totalTime = 0;
 		for (int i = 0; i < _numberOfCores; i++) {
-			ArrayList<AlgorithmNode> currentCore = coreSchedules.get(i);
-			AlgorithmNode algNode = currentCore.get(currentCore.size() - 1);
-			int timeTaken = st.getNodeStartTime(algNodes.indexOf(algNode)) + _dag.getNodeByName(algNode.getNodeName()).getWeight();
+			AlgorithmNode latestAlgNode = latestAlgNodeInSchedules.get(i);
+			int timeTaken = st.getNodeStartTime(algNodes.indexOf(latestAlgNode)) + _dag.getNodeByName(latestAlgNode.getNodeName()).getWeight();
 
 			if (timeTaken > totalTime) {
 				totalTime = timeTaken;
@@ -157,21 +126,18 @@ public class Algorithm {
 	}
 
 	/**
-	 * Finds the corresponding index for accessing/setting the right startTime in a SchedulerTime object.
-	 * @param nodeName - name of the {@code Node} or {@code AlgorithmNode} to find the index for
-	 * @param algNodes - {@code List<AlgorithmNode>} that was originally passed to create the SchedulerTime object
-	 * @param st - {@code SchedulerTime} object to find the index within
-	 * @return - startTime of the given {@code Node/AlgorithmNode}
+	 * Finds and returns the index position of the corresponding {@code AlgorithmNode} within the given {@code List<AlgorithmNode}
+	 * @param node - {@code Node} to find the corresponding index position for
+	 * @param algNodes - {@code List<AlgorithmNode>} to find the index for
+	 * @return the index position of the corresponding {@code AlgorithmNode} object
 	 */
-	private int getStartTimeFromSchedulerTime(String nodeName, List<AlgorithmNode> algNodes, SchedulerTime st) {
-		int index = 0;
+	private int getIndexOfList(Node node, List<AlgorithmNode> algNodes) {
 		for (AlgorithmNode algNode : algNodes) {
-			if (nodeName.equals(algNode.getNodeName())) {
-				index = algNodes.indexOf(algNode);
-				break;
+			if (node.getName().equals(algNode.getNodeName())) {
+				return algNodes.indexOf(algNode);
 			}
 		}
 
-		return st.getNodeStartTime(index);
+		return -1;
 	}
 }
