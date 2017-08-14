dee2a17231dabd6ab40cf6e1a4bf244c7f65e0af
dan-wong <dan_wong@gmail.com>
2017-08-14 13:23:29 +1200

    Daniel/Victor - Added output to file, fixed pom to build with Main.class ammend

--- src/main/java/implementations/io/OutputImp.java
+++ src/main/java/implementations/io/OutputImp.java
@@ -5,6 +5,8 @@ import interfaces.structures.NodeSchedule;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -23,66 +25,73 @@ public class OutputImp implements Output {
 		_inputPath = inputPath;
 		_outputPath = outputPath;
 	}
-	
+
 	public void printOutput() {
+		System.out.println(generateStringOutput());
+	}
+
+	public void outputToFile() {
+		String output = generateStringOutput();
+		String outputPath = _inputPath.substring(0, _inputPath.length() - 4) + "_schedule.dot";
+		try {
+			PrintWriter writer = new PrintWriter(outputPath, "UTF-8");
+			writer.println(output);
+			writer.close();
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String generateStringOutput() {
 		File file = new File (_inputPath);
 		StringBuilder sb = new StringBuilder();
-		
+
 		try {
 			Scanner inputScanner = new Scanner (file);
-			
+
 			//Append "output" to the digraph name.
-			String firstLine = inputScanner.nextLine(); 
+			String firstLine = inputScanner.nextLine();
 			int qutIndex = firstLine.indexOf("\"");
 			sb.append(firstLine.substring(0, qutIndex+1));
 			sb.append("output");
 			sb.append(firstLine.substring(qutIndex+1, qutIndex+2).toUpperCase());
 			sb.append(firstLine.substring(qutIndex+2, firstLine.length()));
 			sb.append(NEWLINE);
-			
+
 			//Loop to read all lines. Only exit if reaches a single line with closing bracket }.
 			while (true) {
 				String line = inputScanner.nextLine();
-				
+
 				//Final line escape.
 				if (line.trim().equals("}")) {
 					sb.append(line);
 					break;
 				}
-				
+
 				String[] lineArray = line.split("\\[");
-				
+
 				//Processing the name (index 1)
-				lineArray[0] = lineArray[0].trim();				
+				lineArray[0] = lineArray[0].trim();
 				if (!lineArray[0].contains("->")) {//For a node
 					String nameOfNode = lineArray[0].trim();
 					NodeSchedule ns = _bestSchedule.get(nameOfNode);
-					
+
 					int sqbkIndex = line.indexOf("]");
 					sb.append(line.substring(0, sqbkIndex));
 					sb.append(",Start=" + ns.getBestStartTime() + ",Processor=" + ns.getBestProcessor());
 					sb.append(line.substring(sqbkIndex, line.length()));
 					sb.append(NEWLINE);
-					
-				}
-				else { //For an arc
+
+				} else { //For an arc
 					sb.append(line);
 					sb.append(NEWLINE);
-				}							
+				}
 			}
-			System.out.println(sb.toString());
 			inputScanner.close();
-			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}		
-	}
-	
-	/**
-	 * TODO: Need to implement write to an output file instead
-	 * of printing to console.
-	 */
-	public void outputToFile(){
-		
+		}
+
+		return sb.toString();
 	}
 }
